763eb82a12da3fff325783846d8dfe966f4bc6f9
josh <joshjj3@gmail.com>
2024-06-12 15:27:51 -0600

    wed commit navigating rows testing

--- excel-parser.go
+++ excel-parser.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	file, err := excelize.OpenFile("C:/Users/User/source/go-repos/chromebook-checkout/div-test.xlsx")
+	file, err := excelize.OpenFile("C:/Users/User/source/go-repos/chromebook-checkout/cli-tool-test.xlsx")
 	if err != nil {
 		println(err)
 		log.Fatal(err)
 	}
 defer file.Close()
 
-for _, row := range excelize.Rows {
-	for _, colCell
+rows,err := file.GetRows("csp") 
+if err != nil {
+	println(err)
+	log.Fatal(err)	
 }
+fmt.Println(rows[10][9])
+reader := 	bufio.NewReader(os.Stdin)
+fmt.Println("Enter colum num: ")
+text, err := reader.ReadString("\n")
+num, err := strconv.Atoi(text)
+if err != nil {
+	fmt.Println(err)
+	log.Fatal(err)
+}
+
+fmt.Println(rows[10][num])
+
 }
