f881f857e0ac1055e441111135760773f098edbe
josh jensen <joshjj3@gmail.com>
2024-06-13 02:23:25 -0600

    first search works implementing id search

--- excel-parser.go
+++ excel-parser.go
@@ -1,38 +1,71 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
+	"time"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	file, err := excelize.OpenFile("C:/Users/User/source/go-repos/chromebook-checkout/cli-tool-test.xlsx")
+	file, err := excelize.OpenFile("J:/source2/go-projects/chromebook-checkout/cli-tool-test.xlsx")
 	if err != nil {
 		println(err)
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-rows,err := file.GetRows("csp") 
-if err != nil {
-	println(err)
-	log.Fatal(err)	
-}
-fmt.Println(rows[10][9])
-reader := 	bufio.NewReader(os.Stdin)
-fmt.Println("Enter colum num: ")
-text, err := reader.ReadString("\n")
-num, err := strconv.Atoi(text)
-if err != nil {
-	fmt.Println(err)
-	log.Fatal(err)
-}
+	rows, err := file.GetRows("csp")
+	if err != nil {
+		println(err)
+		log.Fatal(err)
+	}
 
-fmt.Println(rows[10][num])
+	fmt.Println(rows[10][10])
+	var room string
+	var checkedItems [][]string
+
+	fmt.Println("Enter room number: ")
+	fmt.Scanln(&room)
+	fmt.Println(room)
+
+	for _, row := range rows {
+		for _, colCell := range row {
+			if colCell == room {
+				fmt.Println(row)
+				checkedItems = append(checkedItems, row)
+			}
+		}
+	}
+
+	var itemsCount int = len(checkedItems)
+	var itemsString string = strconv.FormatInt(int64(itemsCount), 10)
+	msg := fmt.Sprintf("\n Checked items count: %s", itemsString)
+	fmt.Println(msg)
+
+	var loop bool = true
+	var search string
+	for loop {
+		fmt.Println("input sn/id: ")
+		fmt.Scanln(&search)
+		if search != "" {
+			for _, row := range rows {
+				if row[2] == search {
+					fmt.Println(row[10])
+				}
+			}
+		} else {
+			println("exiting")
+			time.Sleep(time.Second)
+			c := exec.Command("clear")
+			c.Stdout = os.Stdout
+			c.Run()
+			loop = false
+		}
+	}
 
 }
