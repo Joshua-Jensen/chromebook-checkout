52a78c6d71521d727694656aefeece253be78299
josh <joshjj3@gmail.com>
2024-06-14 14:56:14 -0600

    friday changing some variables starting go routines

--- excel-parser.go
+++ excel-parser.go
@@ -7,34 +7,42 @@ import (
 	"os/exec"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/xuri/excelize/v2"
 )
-
-func main() {
 	type envVariables struct {
 		path          string
-		worksheetName string
+		worksheetNames []string
 	}
+
+func main() {
+
+	var env envVariables
+
+
 	var debug bool = true
 	println("debug mode:", debug)
-	pathChan := make(chan string)
-	go func() {
-		pathChan <- setupEnv()
-	}()
-	path := <-pathChan
 
-	file, err := excelize.OpenFile(path)
+
+	envChan := make(chan envVariables)
+	go func() {
+		envChan <- setupEnv()
+	}()
+	env = <-envChan
+
+
+	file, err := excelize.OpenFile(env.path)
 	if err != nil {
-		println(err)
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	rows, err := file.GetRows("csp")
 	if err != nil {
-		println(err)
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 
@@ -66,11 +74,16 @@ func main() {
 		fmt.Println("input sn/id: ")
 		fmt.Scanln(&search)
 		if search != "" {
+			var loopCount int = 0 
 			for _, row := range rows {
-				if row[2] == search {
+				loopCount++
+				if len(row) >3 {
+					if row[2] == search {
 					fmt.Println(row[10])
+					}
 				}
 			}
+			search = ""
 		} else {
 			println("exiting")
 			time.Sleep(time.Second)
@@ -83,12 +96,15 @@ func main() {
 
 }
 
-func setupEnv() string {
-	var path string
+func setupEnv() envVariables {
+	var env envVariables
+	var worksheetStr string
 	fmt.Println("enter file path: ")
-	fmt.Scanln(&path)
+	fmt.Scanln(&env.path)
 	re := regexp.MustCompile(`\\`)
-	path = re.ReplaceAllLiteralString(path, "/")
-
-	return path
+	env.path = re.ReplaceAllLiteralString(env.path, "/")
+fmt.Println("Enter worksheet names comma separated:")
+fmt.Scanln(&worksheetStr)
+env.worksheetNames = strings.Split(worksheetStr, ",")
+	return env
 }
